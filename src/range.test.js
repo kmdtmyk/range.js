6b87dcf58d856704fc9f46292459fc5d27f990c5
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-10-12 18:30:40 +0900

    Refactoring test code

--- src/range.test.js
+++ src/range.test.js
@@ -6,38 +6,46 @@ test('invalid constructor parameters', () => {
   expect(() => new Range(0, '0')).toThrow()
 })
 
-test('for of', () => {
-  let count = 0
-  for(const i of new Range(0, 5)){
-    count++
-  }
-  expect(count).toBe(6)
+describe('for of', () => {
+
+  test('basic', () => {
+    let count = 0
+    for(const i of new Range(0, 5)){
+      count++
+    }
+    expect(count).toBe(6)
+  })
+
+  test('excludeEnd = true', () => {
+    let count = 0
+    for(const i of new Range(0, 5, true)){
+      count++
+    }
+    expect(count).toBe(5)
+  })
+
 })
 
-test('for of (excludeEnd = true)', () => {
-  let count = 0
-  for(const i of new Range(0, 5, true)){
-    count++
-  }
-  expect(count).toBe(5)
-})
+describe('forEach', () => {
 
-test('forEach', () => {
-  let count = 0
-  new Range(0, 5).forEach(() => count++)
-  expect(count).toBe(6)
-})
+  test('basic', () => {
+    let count = 0
+    new Range(0, 5).forEach(() => count++)
+    expect(count).toBe(6)
+  })
 
-test('forEach (excludeEnd = true)', () => {
-  let count = 0
-  new Range(0, 5, true).forEach(() => count++)
-  expect(count).toBe(5)
-})
+  test('excludeEnd = true', () => {
+    let count = 0
+    new Range(0, 5, true).forEach(() => count++)
+    expect(count).toBe(5)
+  })
 
-test('forEach returns self instance', () => {
-  const range = new Range(0, 5)
-  const result = range.forEach(() => {})
-  expect(result).toBe(range)
+  test('return value is self instance', () => {
+    const range = new Range(0, 5)
+    const result = range.forEach(() => {})
+    expect(result).toBe(range)
+  })
+
 })
 
 test('map', () => {
@@ -46,43 +54,51 @@ test('map', () => {
   expect(result).toEqual(array)
 })
 
-test('step', () => {
-  let count = 0
-  new Range(0, 10).step(2, () => count++)
-  expect(count).toBe(6)
+describe('step', () => {
+
+  test('basic', () => {
+    let count = 0
+    new Range(0, 10).step(2, () => count++)
+    expect(count).toBe(6)
+  })
+
+  test('excludeEnd = true', () => {
+    let count = 0
+    new Range(0, 10, true).step(2, () => count++)
+    expect(count).toBe(5)
+  })
+
+  test('argument error', () => {
+    const range = new Range(0, 5)
+    expect(() => range.step(0, () => {})).toThrow()
+    expect(() => range.step(-1, () => {})).toThrow()
+  })
+
 })
 
-test('step (excludeEnd = true)', () => {
-  let count = 0
-  new Range(0, 10, true).step(2, () => count++)
-  expect(count).toBe(5)
-})
+describe('toArray', () => {
 
-test('step with argument error', () => {
-  const range = new Range(0, 5)
-  expect(() => range.step(0, () => {})).toThrow()
-  expect(() => range.step(-1, () => {})).toThrow()
-})
+  test('basic', () => {
+    const range = new Range(0, 5)
+    const array = [0, 1, 2, 3, 4, 5]
+    expect(range.toArray()).toEqual(array)
+    expect(Array.from(range)).toEqual(array)
+  })
 
-test('toArray', () => {
-  const range = new Range(0, 5)
-  const array = [0, 1, 2, 3, 4, 5]
-  expect(range.toArray()).toEqual(array)
-  expect(Array.from(range)).toEqual(array)
-})
+  test('excludeEnd = true', () => {
+    const range = new Range(0, 5, true)
+    const array = [0, 1, 2, 3, 4]
+    expect(range.toArray()).toEqual(array)
+    expect(Array.from(range)).toEqual(array)
+  })
 
-test('toArray (excludeEnd = true)', () => {
-  const range = new Range(0, 5, true)
-  const array = [0, 1, 2, 3, 4]
-  expect(range.toArray()).toEqual(array)
-  expect(Array.from(range)).toEqual(array)
-})
+  test('string', () => {
+    const range = new Range('a', 'e')
+    const array = ['a', 'b', 'c', 'd', 'e']
+    expect(range.toArray()).toEqual(array)
+    expect(Array.from(range)).toEqual(array)
+  })
 
-test('toArray (string)', () => {
-  const range = new Range('a', 'e')
-  const array = ['a', 'b', 'c', 'd', 'e']
-  expect(range.toArray()).toEqual(array)
-  expect(Array.from(range)).toEqual(array)
 })
 
 test('toString', () => {
